49307d12d1dd4273f997a3b3492cd77c0dd19381
Dominick Washburn <domwashburn@us.ibm.com>
2016-04-01 17:17:48 -0500

    created helper file with tutorial by Stephen Grider

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,35 +1,41 @@
-import _$ from 'jquery';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
 import jsdom from 'jsdom';
+import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
+import chaiJquery from 'chai-jquery';
 
+// make the terminal "run like a browser"
+// same as window.document in browser
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+// same as window in browser
 global.window = global.document.defaultView;
-const $ = _$(window);
+const $ = jquery(global.window);
 
-chaiJquery(chai, chai.util, $);
-
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
+// render react component classes to HTML
+function renderComponent(ComponentClass, props, state) {
+  const componentInstance = TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
       <ComponentClass {...props} />
     </Provider>
   );
 
-  return $(ReactDOM.findDOMNode(componentInstance));
-}
+  return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML (This is where the magic happens)
+};
 
+// helper for simulating events
+// 'this' references the elements passed into the jquery function 
+// $('div').simulate() : this === div
 $.fn.simulate = function(eventName, value) {
   if (value) {
     this.val(value);
   }
   TestUtils.Simulate[eventName](this[0]);
 };
+chaiJquery(chai, chai.util, $)
 
-export {renderComponent, expect};
+export { renderComponent, expect }
